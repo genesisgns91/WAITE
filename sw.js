b690d84125cf6922b91b560bef3e90de67b9137f
genesisgns91 <genesisgns@gmail.com>
2026-08-08 11:43:12 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,18 @@
-const CACHE_NAME = 'waite-v1';
+const CACHE_NAME = 'waite-v2';
 const urlsToCache = [
   './',
   './index.html',
   './estilos.css',
   './tarot.html',
   './revolucao_solar.html',
-  ./transitos_diarios.html'
+  './transitos_diarios.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-maskable-512.png',
+  './apple-touch-icon.png',
+  './favicon.ico',
+  './favicon-32.png'
 ];
 
 self.addEventListener('install', event => {
@@ -15,6 +22,19 @@ self.addEventListener('install', event => {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(nomes =>
+      Promise.all(
+        nomes
+          .filter(nome => nome !== CACHE_NAME)
+          .map(nome => caches.delete(nome))
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
